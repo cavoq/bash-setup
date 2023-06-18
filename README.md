fd4aa1dfd4f63a1c14594df34e3ad694c553270a
David Stromberger <DavidStromberger99@gmx.de>
2023-06-18 18:36:36 +0200

    ip info

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ $ ./<script-name>
 - [install-docker-sh](https://github.com/cavoq/useful-scripts/blob/master/install-docker.sh): Install and configure Docker and Docker Compose for the current user in 1 step.
 - [github-info-sh](https://github.com/cavoq/useful-scripts/blob/master/github-info.sh): Get info of a Github user.
 - [github-backup-sh](https://github.com/cavoq/useful-scripts/blob/master/github-backup.sh): Backup all repositories (including private) of a Github user.
+- [ip-info](https://github.com/cavoq/useful-scripts/blob/master/ip-info.sh): Obtain information about an IP address.
---
+++ ip-info.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# This bash script is used to get the location of a IP address.
+#
+# Author: Cavoq
+#
+# Usage: ./ip-info.sh <ip_address>
+
+IP_API="http://ip-api.com/json"
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./get_location.sh <ip_address>"
+    exit 1
+fi
+
+if ! command -v curl &> /dev/null; then
+    sudo apt install curl -y
+fi
+
+if ! command -v jq &> /dev/null; then
+    sudo apt install jq -y
+fi
+
+if ! command -v awk &> /dev/null; then
+    sudo apt install awk -y
+fi
+
+ip_address="$1"
+
+response=$(curl -s "https://ipinfo.io/$ip_address/json")
+echo "$response" | jq
